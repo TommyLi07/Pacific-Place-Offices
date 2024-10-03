d6ec289116b3085e52679d94f26c26b8185f9723
tommy <yuli9223@colorado.edu>
2024-10-03 17:32:44 +0800

    feat: remove outside scroll bar on customization page

--- src/components/LoadingSpinner/LoadingSpinner.tsx
+++ src/components/LoadingSpinner/LoadingSpinner.tsx
@@ -2,7 +2,7 @@ import { memo } from 'react';
 import { ClipLoader } from 'react-spinners';
 
 export const LoadingSpinner = memo(() => (
-	<div className='w-screen h-screen bg-white flex justify-center items-center'>
+	<div className='w-full h-full bg-white flex justify-center items-center'>
 		<ClipLoader size={32} color='#715e39' />
 	</div>
 ));
